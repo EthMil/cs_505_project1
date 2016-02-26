7673915b6cb4cfde8b0339ed0b3169f2a0e5aeb7
Ethan Miller <ethanmiller2011.em@gmail.com>
2016-02-26 23:38:37 +0000

    modified main to work with cloud 9 mysql server

--- main.py
+++ main.py
@@ -3,21 +3,22 @@
 import MySQLdb
 
 #ask for username and password
-username = raw_input("input UKY username: ")
-password = raw_input("input UKY password: ")
+username = raw_input("input username: ")
+password = raw_input("input password: ")
 database = raw_input("input database name: ")
 
 # Open database connection                                                                                                                           
-db = MySQLdb.connect("delphi.cs.engr.uky.edu",username,password,database )
+# db = MySQLdb.connect("delphi.cs.engr.uky.edu",username,password,database )
+db = MySQLdb.connect("localhost", username, password, database)
 
 # prepare a cursor object using cursor() method                                                                                                      
 cursor = db.cursor()
-
 # execute SQL query using execute() method.                                                                                                          
 cursor.execute("SELECT * FROM roles")
 
 # Fetch a single row using fetchone() method.                                                                                                        
 data = cursor.fetchone()
+print('\n')
 print(data)
 
 # disconnect from server                                                                                                                             
